a2fcaa7e7aa9053b30e26333893e46fccbe8a5ab
andre <andreeyock123@gmail.com>
2025-02-25 16:12:17 +0100

    mise à jour de la classe database_connector.py

--- src/modules/database_connector.py
+++ src/modules/database_connector.py
@@ -1,14 +1,20 @@
-
 from PyQt5.QtWidgets import QMessageBox
 import sqlite3
 
 class DatabaseConnector:
-    def __init__(self):
-        self.connection = None
-        
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None  
+
     def connect(self):
         try:
-            self.connection = sqlite3.connect('sgbd_simulator.db')
+            self.connection = sqlite3.connect(self.db_path)  # Utiliser le bon chemin de la BD
             return True
         except Exception as e:
             return False
+
+    def get_connection(self):
+        """Retourne la connexion SQLite."""
+        if self.connection is None:
+            self.connect()
+        return self.connection
